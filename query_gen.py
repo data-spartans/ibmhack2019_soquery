3cae3bda659c0b49f9409d8122d1d20338b18c7a
Aditya Kumar <kumar.sava.aditya@gmail.com>
2019-06-19 19:03:47 +0530

    Add basic query generator

---
+++ query_gen.py
@@ -0,0 +1,68 @@
+from nltk.tokenize import sent_tokenize
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+class Query():
+    def __init__(self, intitle="", tags="", sortby="", id=None):
+        self.intitle = intitle
+        self.tags = tags
+        self.sortby = sortby
+
+class Answer():
+    def __init__(self, question_text="", accepted=False, votes="", url="", id=None):
+        self.question_text = question_text
+        self.accepted = accepted
+        self.votes = votes
+
+class QueryGenerator():
+    def __init__(self, base_query=""):
+        self.base_query = base_query
+
+    def tokenize(self, text):
+        sentences = sent_tokenize(text)
+        result = []
+        for s in sentences:
+            result.append(word_tokenize(s))
+        return result
+
+    def remove_punctuations(self, tokenized_text):
+        result = []
+        for s in tokenized_text:
+            new_sent = []
+            for word in s:
+                if len(word) > 1 or str.isalnum(word):
+                    new_sent.append(word)
+            result.append(new_sent)
+        return result
+
+    def remove_stopwords(self, tokenized_text):
+        result = []
+        stop_words = set(stopwords.words("english"))
+        for s in tokenized_text:
+            new_sent = []
+            for word in s:
+                if word not in stop_words:
+                    new_sent.append(word)
+            result.append(new_sent)
+        return result
+
+    def detokenize(self, tokenized_text):
+        return ' '.join([' '.join(sent) for sent in tokenized_text])
+
+    def generate(self):
+        queries = []
+        queries.append(Query(intitle=self.base_query))
+        tokenized_base = self.tokenize(self.base_query)
+        no_puncts = self.remove_punctuations(tokenized_base)
+        no_stopwords = self.remove_stopwords(no_puncts)
+        queries.append(Query(intitle = self.detokenize(no_puncts)))
+        queries.append(Query(intitle = self.detokenize(no_stopwords)))
+        return queries
+
+if __name__ == '__main__':
+    q = input("Enter your query: ")
+    qgen = QueryGenerator(q)
+    alternates = qgen.generate()
+    for q in alternates:
+        print(q.intitle)
+
