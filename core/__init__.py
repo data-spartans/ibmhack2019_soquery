e57ba3f4cccd2b46f87f01c1ab3ecf79390d0d13
Aditya Kumar <kumar.sava.aditya@gmail.com>
2019-07-01 22:30:47 +0530

    Fix bug in core __init__ and update .gitignore

--- core/__init__.py
+++ core/__init__.py
@@ -2,13 +2,13 @@ import os, nltk
 
 file_dir = os.path.dirname(os.path.abspath(__file__))
 data_dir = os.path.join(file_dir, 'nltk_data')
-nltk_packages = [{'name': 'stopwords', 'path': 'corpora/stopwords'},
-                 {'name': 'punkt', 'path': 'tokenizers/punkt'}]
+nltk_packages = [{'id': 'stopwords', 'name': 'corpora/stopwords'},
+                 {'id': 'punkt', 'name': 'tokenizers/punkt'}]
 nltk.data.path.append(data_dir)
 
 for package in nltk_packages:
     try:
-        nltk.data.find(package['path'])
+        nltk.data.find(package['name'])
     except LookupError:
-        print('-> Installing NLTK package:', package)
-        nltk.download(package, download_dir=data_dir)
+        print('-> Installing NLTK package:', package['name'])
+        nltk.download(package['id'], download_dir=data_dir)
